4c570878374b402f70bd3495d6a0005d402a52ae
jackrobert0220 <jackrobert0220@gmail.com>
2022-03-25 17:23:52 -0700

    migrated users

--- main_app/models.py
+++ main_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 
@@ -21,6 +22,9 @@ class Poke(models.Model):
     img = models.CharField(max_length=250)
     number = models.IntegerField()
     type = models.CharField(max_length=10, choices = TYPE_CHOICES)
+    #USER implementation
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
