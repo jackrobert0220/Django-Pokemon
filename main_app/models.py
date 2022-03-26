635f5d972290abbda10c0912c5b3d4a45f2633b4
jackrobert0220 <jackrobert0220@gmail.com>
2022-03-25 19:31:51 -0700

    about to implement abilities

--- main_app/models.py
+++ main_app/models.py
@@ -2,6 +2,25 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+# TYPE_CHOICES = (
+#     ("e", "electric"),
+#     ("d", "dragon"),
+#     ("f", "fire"),
+#     ("g", "ghost"),
+#     ("n", "normal"),
+#     ("p", "plant"),
+#     ("ps", "psychic"),
+#     ("r", "rock"),
+#     ("w", "water"),
+# )
+
+
+# class PokeAbility(models.Model):
+#     name = models.Charfield(max_length=100)
+#     type = models.Charfield(max_length=10, choices = TYPE_CHOICES)
+
+#     def __str__(self):
+#         return self.name
 
 TYPE_CHOICES = (
     ("e", "electric"),
@@ -24,7 +43,7 @@ class Poke(models.Model):
     type = models.CharField(max_length=10, choices = TYPE_CHOICES)
     #USER implementation
     user = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
